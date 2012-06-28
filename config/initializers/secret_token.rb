5bb52dec73da09de83ce804100692e0aa4d0fe44
Neal Schexnider <nschexni@tulane.edu>
2012-06-28 13:49:27 -0500

    initialized Neal Website

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NealWebsite::Application.config.secret_token = 'f8144a9974927ff992600d4d05bbf0b26c9280d3faa4d0eb45883440ba56a2719becfd652407e574401b199b2b9373dfb54ad89aeb7fefc5731fda146eed2da1'
